4b278f68bc7f4fdfe65586c152b89bc26e750039
unknown <a27657060@gmail.com>
2024-06-13 22:53:45 +0800

    feat: add content to ProfileTab

--- src/components/Profile/ProfileTab.jsx
+++ src/components/Profile/ProfileTab.jsx
@@ -1,6 +1,47 @@
+import { Tabs, TabList, Tab, TabIndicator, HStack, Text, Box } from '@chakra-ui/react'
+import { FaRegCommentDots, FaRegHeart } from 'react-icons/fa'
+import { FiGrid } from 'react-icons/fi'
+import { GoBookmark } from 'react-icons/go'
+
 const ProfileTab = () => {
   return (
-    <div>ProfileTab</div>
+    <Tabs position='relative' variant='unstyled' borderBottom={'1px solid'} borderColor={'gray.300'} align='center' mt={10} >
+      <TabList color={'gray.400'} >
+        <Tab _selected={{ color: 'blue.500' }}>
+          <HStack>
+            <Box display={{ base: 'flex', md: 'none' }}>
+              <FiGrid size={25}/>
+            </Box>
+            <Text display={{ base: 'none', md: 'flex' }}>分享的餐廳</Text>
+          </HStack>
+        </Tab>
+        <Tab _selected={{ color: 'blue.500' }}>
+          <HStack>
+            <Box display={{ base: 'flex', md: 'none' }}>
+              <FaRegHeart size={25} />
+            </Box>
+            <Text display={{ base: 'none', md: 'flex' }}>喜歡的餐廳</Text>
+          </HStack>
+        </Tab>
+        <Tab _selected={{ color: 'blue.500' }}>
+          <HStack>
+            <Box display={{ base: 'flex', md: 'none' }}>
+              <GoBookmark size={25} />
+            </Box>
+            <Text display={{ base: 'none', md: 'flex' }}>收藏的餐廳</Text>
+          </HStack>
+        </Tab>
+        <Tab _selected={{ color: 'blue.500' }}>
+          <HStack>
+            <Box display={{ base: 'flex', md: 'none' }}>
+              <FaRegCommentDots size={25}/>
+            </Box>
+            <Text display={{ base: 'none', md: 'flex' }}>留言過的餐廳</Text>
+          </HStack>
+        </Tab>
+      </TabList>
+      <TabIndicator mt='-1.5px' height='2px' bg='blue.500' borderRadius='1px' />
+      </Tabs>
   )
 }
 
