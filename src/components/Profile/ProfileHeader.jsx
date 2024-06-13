bd0e7dc5a86f6a06d63099d15465f14c4ddcaf52
unknown <a27657060@gmail.com>
2024-06-13 22:28:59 +0800

    add content to ProfileHeader

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,6 +1,35 @@
+import { Avatar, Button, Flex, Heading, Stack, Text, VStack, HStack } from '@chakra-ui/react'
+import { useParams } from 'react-router-dom'
+
 const ProfileHeader = () => {
+  const userId = Number(useParams().userId)
+  const authId = 1
   return (
-    <div>ProfileHeader</div>
+    <Stack>
+      <Flex alignItems={{ base: 'center' }} direction={{ base: 'column', md: 'row' }} justifyContent={{ base: 'none', md: 'center' }} gap={{ base: 3, md: 10 }}>
+        <Avatar name='user' size={{ base: 'xl', md: '2xl' }} />
+        <VStack align={{ base: 'center', md: 'flex-start' }}>
+          <Heading fontSize={'2xl'}>Username</Heading>
+          {
+            userId === authId
+              ? (
+              <Button colorScheme='blue'>Edit Profile</Button>
+                )
+              : (
+              <Button colorScheme='blue'>Follow</Button>
+                )
+          }
+          <HStack spacing={5}>
+            <Text>3 posts</Text>
+            <Text>4 followers</Text>
+            <Text>3 followings</Text>
+          </HStack>
+          <Text>
+            yoiejpof;pjdgjsg[hhhwhgwuhapwoh]
+          </Text>
+        </VStack>
+      </Flex>
+    </Stack>
   )
 }
 
