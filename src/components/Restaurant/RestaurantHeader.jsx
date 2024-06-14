7a7cb0825a29b0f57e2e531cd59e35ccc3903adf
unknown <a27657060@gmail.com>
2024-06-14 11:24:28 +0800

    feat: add content to RestaurantHeader

--- src/components/Restaurant/RestaurantHeader.jsx
+++ src/components/Restaurant/RestaurantHeader.jsx
@@ -1,6 +1,19 @@
+import { Avatar, Button, HStack, Text, VStack } from '@chakra-ui/react'
+
 const RestaurantHeader = () => {
+  const createdBy = 1
+  const authId = 2
   return (
-    <div>RestaurantHeader</div>
+    <HStack spacing={5} borderBottom={'1px solid'} borderColor={'gray.300'} width={'100%'} pb={5}>
+      <Avatar src='/cover.jpg' size={'lg'} />
+      <VStack align='flex-start'>
+        <HStack spacing={5}>
+          <Text>User1</Text>
+          { createdBy === authId ? (<Button colorScheme='green'>編輯餐廳</Button>) : (<Button colorScheme='blue'> follow</Button>)}
+        </HStack>
+        <Text color={'gray.500'}>分享於2021/06/01 14:30</Text>
+      </VStack>
+    </HStack>
   )
 }
 
