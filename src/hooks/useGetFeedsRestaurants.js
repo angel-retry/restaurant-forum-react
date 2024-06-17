a57964677e4a205ab4fb31ff69697f258baa7525
unknown <a27657060@gmail.com>
2024-06-17 23:12:46 +0800

    feat: create useGetFeedsRestaurants to fetch data

---
+++ src/hooks/useGetFeedsRestaurants.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+import useAuthTokenStore from '../store/authTokenStore'
+import baseURL from '../config/apiConfig'
+import axios from 'axios'
+
+const useGetFeedsRestaurants = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const authToken = useAuthTokenStore(state => state.authToken)
+  const [feedsRestaurants, setFeedsRestaurants] = useState([])
+
+  const URL = `${baseURL}/restaurants/feeds`
+
+  useEffect(() => {
+    setIsLoading(true)
+    const getFeedsRestaurants = () => {
+      axios
+        .get(URL, {
+          headers: {
+            Authorization: `Bearer ${authToken}`
+          }
+        })
+        .get(res => {
+          const { feedsRestaurants } = res.data
+          setFeedsRestaurants(feedsRestaurants)
+        })
+        .catch(err => console.error(err))
+        .finally(() => {
+          setIsLoading(false)
+        })
+    }
+
+    if (authToken)getFeedsRestaurants()
+  }, [authToken])
+
+  return { isLoading, feedsRestaurants }
+}
+
+export default useGetFeedsRestaurants
