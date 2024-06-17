6c5ff8a22831c964256597bb207400f0365af3e9
unknown <a27657060@gmail.com>
2024-06-17 16:47:49 +0800

    feat: adding no data alert to Restaurants

--- src/components/Restaurants/Restaurants.jsx
+++ src/components/Restaurants/Restaurants.jsx
@@ -1,10 +1,14 @@
-import { Grid, GridItem } from '@chakra-ui/react'
+import { Grid, GridItem, Heading, VStack } from '@chakra-ui/react'
 import Restaurant from './Restaurant'
+import { FaRegFileExcel } from 'react-icons/fa'
 const Restaurants = ({ restaurants }) => {
   const restaurantsData = restaurants
   return (
     <>
-      <Grid
+      {
+        restaurantsData.length > 0
+          ? (
+          <Grid
             templateRows='repeat(1, 1fr)'
             templateColumns={{
               sm: 'repeat(1, 1fr)',
@@ -13,15 +17,24 @@ const Restaurants = ({ restaurants }) => {
             gap={6}
           >
             {
-              restaurantsData.map(restaurant => (
+               restaurantsData.map(restaurant => (
                 <GridItem w='100%' key={restaurant.id} >
                   <Restaurant restaurant={restaurant}/>
                 </GridItem>
 
-              ))
+               ))
             }
 
           </Grid>
+            )
+          : (
+            <VStack color={'gray.500'} spacing={10} py={100}>
+              <FaRegFileExcel size={150} />
+              <Heading fontSize={'2xl'}>沒有餐廳資料QQ</Heading>
+            </VStack>
+            )
+      }
+
     </>
 
   )
