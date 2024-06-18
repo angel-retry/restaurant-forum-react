2b40f02b44bf08d96592f249ec8f21e1fd0e994c
unknown <a27657060@gmail.com>
2024-06-18 16:24:13 +0800

    fetch data to ProfilePosts and ProfilePost

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,29 +1,14 @@
-import { Box, Card, Heading, HStack, Image, Tag, Text, VStack } from '@chakra-ui/react'
-import { FaRegCommentDots, FaRegHeart } from 'react-icons/fa'
-import { GoBookmark } from 'react-icons/go'
+import { Box, Card, Heading, Image, Tag, VStack } from '@chakra-ui/react'
 
-const ProfilePost = () => {
+const ProfilePost = ({ restaurant }) => {
   return (
     <Card >
       <Box h={{ sm: '250px', md: '350px' }}>
-        <Image src='/cover.jpg' w={'100%'} h={'100%'} objectFit={'cover'} />
+        <Image src={restaurant.image} w={'100%'} h={'100%'} objectFit={'cover'} />
       </Box>
       <VStack align={'flex-start'} p={3}>
-        <HStack w={'100%'} justify={'space-between'}>
-          <HStack>
-            <HStack>
-              <FaRegHeart size={25} />
-              <Text>3</Text>
-            </HStack>
-              <HStack>
-              <FaRegCommentDots size={25}/>
-              <Text>3</Text>
-            </HStack>
-          </HStack>
-          <GoBookmark size={25} />
-        </HStack>
-        <Heading fontSize={'2xl'}>Restaurant name</Heading>
-        <Tag colorScheme={'teal'} size={'md'} variant='solid' py={2}>Sample Tag</Tag>
+        <Heading fontSize={'2xl'}>{restaurant.name}</Heading>
+        <Tag colorScheme={'teal'} size={'md'} variant='solid' py={2}>{restaurant.Category.name}</Tag>
       </VStack>
     </Card>
   )
--- src/components/Profile/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts.jsx
@@ -1,14 +1,39 @@
 import { Grid, Stack } from '@chakra-ui/react'
-
 import ProfilePost from './ProfilePost'
+import useTabStore from '../../store/tabStore'
+import useUserProfileStore from '../../store/userProfileStore'
+import { useEffect, useState } from 'react'
 
 const ProfilePosts = () => {
+  const currentProfileTab = useTabStore(state => state.currentProfileTab)
+  const userProfile = useUserProfileStore(state => state.userProfile)
+  const [restaurants, setRestaurants] = useState([])
+
+  useEffect(() => {
+    if (userProfile) {
+      switch (currentProfileTab) {
+        case 'saved':
+          setRestaurants(userProfile.SavedRestaurants)
+          break
+        case 'liked':
+          setRestaurants(userProfile.LikedRestaurants)
+          break
+        case 'created':
+          setRestaurants(userProfile.CreatedRestaurants)
+          break
+        default:
+          setRestaurants(userProfile.CommentedRestaurants)
+          break
+      }
+    }
+  }, [currentProfileTab, userProfile])
+
   return (
     <Stack px={3} mt={5}>
       <Grid templateColumns={{ sm: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)' }} gap={5}>
-        {Array.from({ length: 10 }, (_, i) => (
-          <ProfilePost key={i} />
-        ))}
+        {
+          restaurants.map(restaurant => (<ProfilePost key={restaurant.id} restaurant={restaurant} />))
+        }
       </Grid>
     </Stack>
   )
