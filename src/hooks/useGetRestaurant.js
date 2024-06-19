3e002147b2174c8ffd5021821c6790f29a88b0c4
unknown <a27657060@gmail.com>
2024-06-19 15:53:17 +0800

    feat: create useGetRestaurant to get a restaurant data

---
+++ src/hooks/useGetRestaurant.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import useRestaurantsStore from '../store/restaurantsStore'
+import baseURL from '../config/apiConfig'
+import axios from 'axios'
+import useAuthTokenStore from '../store/authTokenStore'
+
+const useGetRestaurant = (restaurantId) => {
+  const [isLoading, setIsLoading] = useState(true)
+  const restaurant = useRestaurantsStore(state => state.restaurant)
+  const setRestaurant = useRestaurantsStore(state => state.setRestaurant)
+  const URL = `${baseURL}/restaurants/${restaurantId}`
+  const authToken = useAuthTokenStore(state => state.authToken)
+
+  useEffect(() => {
+    setIsLoading(true)
+    const getRestaurant = () => {
+      axios
+        .get(URL, {
+          headers: {
+            Authorization: `Bearer ${authToken}`
+          }
+        })
+        .then(res => {
+          const { restaurant } = res.data
+          setRestaurant(restaurant)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+        .finally(() => {
+          setIsLoading(false)
+        })
+    }
+
+    if (authToken) getRestaurant()
+  }, [authToken, restaurantId, setRestaurant])
+
+  return { isLoading, restaurant }
+}
+
+export default useGetRestaurant
--- src/store/restaurantsStore.js
+++ src/store/restaurantsStore.js
@@ -2,7 +2,9 @@ import { create } from 'zustand'
 
 const useRestaurantsStore = create(set => ({
   restaurants: [],
-  setRestaurants: restaurants => set({ restaurants })
+  setRestaurants: restaurants => set({ restaurants }),
+  restaurant: null,
+  setRestaurant: restaurant => set({ restaurant })
 }))
 
 export default useRestaurantsStore
