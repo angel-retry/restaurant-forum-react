c034384fc3040ffa17a533c0749c2788ef4c198e
unknown <a27657060@gmail.com>
2024-06-20 15:12:03 +0800

    feat: implement save/unSave & like/unLike to RestaurantPage

--- src/components/Restaurant/RestaurantInfo.jsx
+++ src/components/Restaurant/RestaurantInfo.jsx
@@ -1,8 +1,15 @@
-import { Box, Flex, Heading, HStack, Image, Tag, Text, VStack, Wrap } from '@chakra-ui/react'
-import { FaRegBookmark, FaRegCommentDots, FaRegHeart } from 'react-icons/fa'
+import { Box, Button, Flex, Heading, HStack, Image, Tag, Text, VStack, Wrap } from '@chakra-ui/react'
+import { FaHeart, FaRegBookmark, FaRegCommentDots, FaRegHeart } from 'react-icons/fa'
 import { RiMapPin2Fill } from 'react-icons/ri'
+import useSaveRestaurant from '../../hooks/useSaveRestaurant'
+import useLikeRestaurant from '../../hooks/useLikeRestaurant'
+import { GoBookmark, GoBookmarkFill } from 'react-icons/go'
 
 const RestaurantInfo = ({ restaurant }) => {
+  const { isLoading, postLike, isLiked, likes } = useLikeRestaurant(restaurant)
+
+  const { isLoading: isSaving, postSave, isSaved } = useSaveRestaurant(restaurant)
+
   return (
     <VStack width={'100%'} >
       <Flex flexDir={{ base: 'column', md: 'row' }} width={'100%'} gap={2} borderBottom={'1px solid'} borderColor={'gray.300'} py={5}>
@@ -12,13 +19,21 @@ const RestaurantInfo = ({ restaurant }) => {
         <VStack flex={{ base: 'none', md: 1 }} align={'flex-start'} alignSelf={{ base: 'none', md: 'center' }} spacing={{ base: 2, md: 5 }} p={3}>
           <HStack w={'100%'} justify={'space-between'}>
             <Heading>{restaurant.name}</Heading>
-            <FaRegBookmark size={28} />
+            <Button variant={'ghost'} _hover={{ bg: 'transparant' }} p={0} onClick={postSave} isLoading={isSaving}>
+                {
+                  isSaved ? <GoBookmarkFill size={40} /> : <GoBookmark size={40} />
+                }
+            </Button>
           </HStack>
           <Tag colorScheme={'teal'} size={'md'} variant='solid' py={2}>{restaurant.Category.name}</Tag>
           <HStack>
-            <HStack spacing={2}>
-              <FaRegHeart size={22} />
-              <Text fontSize={20}>{restaurant.LikedUsers.length}</Text>
+            <HStack spacing={0}>
+              <Button variant={'ghost'} _hover={{ bg: 'transparant' }} p={0} onClick={postLike} isLoading={isLoading}>
+                {
+                  isLiked ? <FaHeart size={22} color='red' /> : <FaRegHeart size={22} />
+                }
+              </Button>
+              <Text fontSize={20}>{likes}</Text>
             </HStack>
             <HStack spacing={2}>
               <FaRegCommentDots size={22} />
