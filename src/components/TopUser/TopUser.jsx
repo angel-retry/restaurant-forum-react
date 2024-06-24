a0087f092b99868fa6441b58171fccd72b58d473
unknown <a27657060@gmail.com>
2024-06-24 13:00:34 +0800

    feat: add Spinner to TopUsers

--- src/components/TopUser/TopUser.jsx
+++ src/components/TopUser/TopUser.jsx
@@ -3,10 +3,10 @@ import useFollowUser from '../../hooks/useFollowUser'
 import { Link as RouterLink } from 'react-router-dom'
 
 const TopUser = ({ user }) => {
-  const { followersCount, isFollowed, handleFollowUser, isLoading } = useFollowUser(user)
+  const { followersCount, isFollowed, handleFollowUser, isLoading: isFollowing } = useFollowUser(user)
 
   return (
-    <Card maxW={'300px'} w={'100%'} boxShadow={'lg'} variant={'outline'} >
+    <Card maxW={'300px'} w={'100%'} boxShadow={'lg'} variant={'outline'}>
       <VStack p={10} spacing={5}>
         <Link as={RouterLink} to={`/users/${user.id}`}>
           <Avatar src={user.avatar} name='user' size={'lg'} />
@@ -16,7 +16,7 @@ const TopUser = ({ user }) => {
           <Heading fontSize={'2xl'}>{user.name}</Heading>
         </Link>
         <HStack spacing={'20px'} justify={'center'} w={'100%'} fontWeight={'bold'}>
-          <VStack flex={1} >
+          <VStack flex={1}>
             <Text>{followersCount}</Text>
             <Text>粉絲</Text>
           </VStack>
@@ -26,10 +26,8 @@ const TopUser = ({ user }) => {
             <Text>貼文</Text>
           </VStack>
         </HStack>
-        <Button colorScheme='blue' onClick={handleFollowUser} isLoading={isLoading}>
-          {
-            isFollowed ? '取消追蹤' : '追蹤'
-          }
+        <Button colorScheme='blue' onClick={handleFollowUser} isLoading={isFollowing}>
+          {isFollowed ? '取消追蹤' : '追蹤'}
         </Button>
       </VStack>
     </Card>
--- src/pages/TopUsersPage/TopUsersPage.jsx
+++ src/pages/TopUsersPage/TopUsersPage.jsx
@@ -1,4 +1,4 @@
-import { Grid, Heading, Stack } from '@chakra-ui/react'
+import { Grid, Heading, Spinner, Stack, VStack } from '@chakra-ui/react'
 import useGetTopUsers from '../../hooks/useGetTopUsers'
 import TopUser from '../../components/TopUser/TopUser'
 
@@ -9,15 +9,22 @@ const TopUsersPage = () => {
     <Stack maxW={1440} mx={'auto'} w={'100%'} px={3}>
       <Heading>Top Users</Heading>
       {
-        !isLoading && (
+        isLoading
+          ? (
+            <VStack w={'100%'} h={'100%'} alignItems={'center'}>
+              <Spinner />
+            </VStack>
+            )
+          : (
           <Grid templateColumns="repeat(auto-fill, minmax(300px, 1fr))" justifyItems={'center'} gap={10} >
-            {
-              topUsers.map(user => (
-                <TopUser key={user.id} user={user} />
-              ))
-            }
-          </Grid>
-        )
+          {
+            topUsers.map(user => (
+              <TopUser key={user.id} user={user} isLoading={isLoading}/>
+
+            ))
+          }
+        </Grid>
+            )
       }
 
     </Stack>
