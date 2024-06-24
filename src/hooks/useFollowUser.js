55766d1b2a3401e97aa7e739f36bc11f6ff81e60
unknown <a27657060@gmail.com>
2024-06-24 10:29:54 +0800

    feat: create useFollowUser to follow/unfollower user

--- src/hooks/useFollowUser.js
+++ src/hooks/useFollowUser.js
@@ -1,7 +1,69 @@
+import { useState } from 'react'
+import useAuthTokenStore from '../store/authTokenStore'
+import baseURL from '../config/apiConfig'
+import axios from 'axios'
+
 const useFollowUser = (user) => {
-  return (
-    <div>useFollowUser</div>
-  )
+  const [isLoading, setIsLoading] = useState(false)
+  const authUser = useAuthTokenStore(state => state.authUser)
+
+  const [followersCount, setFollowersCount] = useState(user.Followers.length)
+  const [isFollowed, setIsFollowed] = useState(user.Followers.includes(authUser.id))
+
+  const authToken = useAuthTokenStore(state => state.authToken)
+
+  const URL = `${baseURL}/following/${user.id}`
+
+  const handleFollowUser = () => {
+    if (isLoading) return
+    if (!authToken) return
+    if (!user.id) return
+
+    setIsLoading(true)
+    if (isFollowed) {
+      axios
+        .delete(URL, {
+          headers: {
+            Authorization: `Bearer ${authToken}`
+          }
+        })
+        .then(res => {
+          const { deleteFollowship } = res.data
+          if (deleteFollowship) {
+            setIsFollowed(false)
+            setFollowersCount(followersCount - 1)
+          }
+        })
+        .catch(err => {
+          console.log(err)
+        })
+        .finally(() => {
+          setIsLoading(false)
+        })
+    } else {
+      axios
+        .post(URL, {
+          headers: {
+            Authorization: `Bearer ${authToken}`
+          }
+        })
+        .then(res => {
+          const { newFollowship } = res.data
+          if (newFollowship) {
+            setIsFollowed(true)
+            setFollowersCount(followersCount + 1)
+          }
+        })
+        .catch(err => {
+          console.log(err)
+        })
+        .finally(() => {
+          setIsLoading(false)
+        })
+    }
+  }
+
+  return { followersCount, isFollowed, handleFollowUser, isLoading }
 }
 
 export default useFollowUser
