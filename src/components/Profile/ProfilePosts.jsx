402b702d3837f2f084fb672d262e9aea3abb6677
unknown <a27657060@gmail.com>
2024-06-25 10:36:42 +0800

    feat: add no restaurants message to ProfilePosts

--- src/components/Profile/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts.jsx
@@ -1,8 +1,9 @@
-import { Grid, Stack } from '@chakra-ui/react'
+import { Grid, Stack, Text, VStack } from '@chakra-ui/react'
 import ProfilePost from './ProfilePost'
 import useTabStore from '../../store/tabStore'
 import useUserProfileStore from '../../store/userProfileStore'
 import { useEffect, useState } from 'react'
+import { BsEmojiSurprise } from 'react-icons/bs'
 
 const ProfilePosts = () => {
   const currentProfileTab = useTabStore(state => state.currentProfileTab)
@@ -30,6 +31,16 @@ const ProfilePosts = () => {
 
   return (
     <Stack px={3} mt={5}>
+      {
+        restaurants.length === 0 && (
+          <VStack p={10} spacing={10}>
+            <BsEmojiSurprise size={50} color='gray' />
+            <Text fontSize={25} color='gray'>
+              目前沒有任何一間餐廳!
+            </Text>
+          </VStack>
+        )
+      }
       <Grid templateColumns={{ sm: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)', lg: 'repeat(4, 1fr)' }} gap={5} >
         {
           restaurants.map(restaurant => (<ProfilePost key={restaurant.id} restaurant={restaurant} />))
