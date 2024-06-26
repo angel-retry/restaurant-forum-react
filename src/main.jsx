ba76f1af0b6b5cde8f2ab94b0dd9aa18f07b192d
unknown <a27657060@gmail.com>
2024-06-26 14:08:32 +0800

    feat: add color mode scrollbar to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,23 @@ const styles = {
   global: (props) => ({
     body: {
       bg: mode('grey.100', '#000')(props), // props.colorMode === "light" ? 'grey.100': '#000'
-      color: mode('grey.800', 'whiteAlpha.900')(props)
+      color: mode('grey.800', 'whiteAlpha.900')(props),
+      '::-webkit-scrollbar': {
+        width: '6px'
+      }
+    },
+    color: mode('grey.800', 'whiteAlpha.900')(props),
+    '::-webkit-scrollbar': {
+      width: '6px'
+    },
+    '::-webkit-scrollbar-track': {
+      background: mode('#e6e6e6', 'gray.900')(props)
+    },
+    '::-webkit-scrollbar-thumb': {
+      background: mode('#aaaaaa', 'gray.700')(props)
+    },
+    '::-webkit-scrollbar-thumb:hover': {
+      background: mode('#8b8b8b', 'gray.800')(props)
     }
   })
 }
