e33c6026e32ec871827f1f3a128291534af031e6
unknown <a27657060@gmail.com>
2024-07-09 09:12:32 +0800

    feat: remove Link to Comment componenet

--- src/components/Comments/Comment.jsx
+++ src/components/Comments/Comment.jsx
@@ -5,11 +5,11 @@ import { timeAgo } from '../../utils/timeAgo'
 const Comment = ({ comment }) => {
   return (
     <HStack w={'100%'} align={'flex-start'} borderBottom={'1px solid'} borderColor={'gray.300'} py={5} spacing={5} alignItems={'center'}>
-      <Link as={RouterLink} to={`/users/${comment.User.id}`} _hover={{ textDecoration: 'none' }}>
+      <Link as={RouterLink} _hover={{ textDecoration: 'none' }}>
         <Avatar src={comment.avatar} name='user' size={'lg'} />
       </Link>
       <VStack align={'flex-start'}>
-        <Link as={RouterLink} to={`/users/${comment.User.id}`} _hover={{ textDecoration: 'none' }}>
+        <Link as={RouterLink} _hover={{ textDecoration: 'none' }}>
           <Text>{comment.username}</Text>
         </Link>
         <Text>{comment.text}</Text>
